665cdbb13ec17c9a061da37bfe59edbd2c2d4a03
ulisespallares888 <ulisespallares8@gmail.com>
2024-12-19 19:02:42 -0300

    Primer commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+
+import SintaxisBasicaDeJava.Hola_Mundo;
+class Main {
+    public static void main(String[] args) {
+    Hola_Mundo.holaMundo();
+    }
+}
---
+++ SintaxisBasicaDeJava/Hola_Mundo.java
@@ -0,0 +1,6 @@
+package SintaxisBasicaDeJava;
+public class Hola_Mundo {
+    public static void holaMundo(){
+        System.out.println("Hola mundo");
+    }
+}
