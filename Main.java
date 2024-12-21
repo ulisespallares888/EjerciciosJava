84208aa0b68b8492a8f1d31c333faaca5125270b
ulisespallares888 <ulisespallares8@gmail.com>
2024-12-21 02:31:45 -0300

    Factorial

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@
 import SintaxisBasicaDeJava.*;
 class Main {
     public static void main(String[] args) {
-     Divisores.divisores(100);
+     Factorial.factorear(32);
     }
 }
---
+++ SintaxisBasicaDeJava/Factorial.java
@@ -0,0 +1,29 @@
+package SintaxisBasicaDeJava;
+
+import java.util.ArrayList;
+
+public class Factorial {
+    public static void factorear(int valor){
+
+        int base, divisor = 2;
+        if (valor % 2 == 0){
+            base = valor / 2;
+        }else {
+            base = (valor + 1) / 2;
+        }
+        int cantDivisines  = 0;
+        while (divisor <= base){
+            cantDivisines++;
+            if (valor % divisor == 0) {
+                int potencia = 0;
+                while (valor % divisor == 0) {
+                    valor = valor / divisor;
+                    potencia++;
+                }
+                System.out.println(divisor+"^"+potencia);
+            }
+            divisor++;
+        }
+        System.out.println(cantDivisines);
+    }
+}
