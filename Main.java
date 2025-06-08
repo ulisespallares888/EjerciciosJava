95cee47760e7c7519997ad46a23518d0cf9be874
ulisespallares888 <ulisespallares8@gmail.com>
2025-06-08 19:40:42 -0300

    Comentario en el main

--- Main.java
+++ Main.java
@@ -4,13 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> listaDeEdades = new ArrayList<>(List.of(3,4,3,5,6,3,2,1,1,6,2));
-        LinkedList<String> cola = new LinkedList<>(
-                Arrays.asList("ulises","pablo","juan","matias","tobias","carlos")
-        );
-        List<Integer> lista = Arrays.asList(4,6,3,8,9,7,2,10,1,5);
-        Integer longitud = 10;
 
-        System.out.println("Valor faltante " + NumeroFaltante.valorFaltante(lista,longitud));
+        System.out.println("asd");
     }
 }
